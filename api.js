27de932394fee0d90d1a0a79392b409774e05cab
CommanderS <sanders_de_vries@hotmail.com>
2024-02-03 22:54:29 +0100

    Added support for requesting json.

--- api.js
+++ api.js
@@ -1,9 +1,26 @@
 // api.js
 const http = require('http');
 const port = 3001;
-const server = http.createServer((req, res) => {
+
+const request = require('request');
+let url = "https://data-api.binance.vision/api/v3/ticker/24hr";
+let options = { json: true };
+
+const server = http.createServer((req, res) =>
+{
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'Node.js API' }));
+  request(url, options, (error, res, body) => {
+    if (error) {
+      console.log(error)
+    };
+
+    if (!error && res.statusCode == 200) {
+      console.log(body);
+      res.end(body);
+    };
+  });
+  
+  /*res.end(JSON.stringify({ message: 'Node.js API' }));*/
 });
 server.listen(port, () => {
   console.log(`API server is running on http://localhost:${port}`);
