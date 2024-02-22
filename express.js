536db195d190edad04a02ea57180fad76dd7cc66
CommanderS <sanders_de_vries@hotmail.com>
2024-02-22 22:16:57 +0100

    Multi-page retrieval from CoinGecko.

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const https = require('https');
 
 let url = "";
 let options = {
@@ -12,7 +13,11 @@ let host_coingecko = "https://api.coingecko.com/api/";
 
 app.get('/', (req, res) => {
   let paths = [];
+  paths.push("/coingecko/markets/btc/");
   paths.push("/coingecko/markets/btc/1/");
+  paths.push("/coingecko/markets/usd/");
+  paths.push("/coingecko/markets/usd/1/");
+  paths.push("/coingecko/markets/usd/2/");
   paths.push("/binance/ticker/24hr");
   paths.push("/binance/klines/BTCUSDT/30m/1000/");
   let output = "<ul>"
@@ -62,207 +67,161 @@ app.get('/binance/klines/BTCUSDT/30m/1000/', (req, res) => {
   });
 });
 
-app.get('/coingecko/markets/btc/1/', (req, res) => {
-  const request = require('request');
-  url = host_coingecko + "v3/coins/markets?vs_currency=btc&order=market_cap_desc&per_page=250&page=1&sparkline=false";
+//function getCoinGeckoMarkets(page, vs_currency) {
+//  const request = require('request');
+//  let url = host_coingecko + "v3/coins/markets?vs_currency=" + vs_currency + "&order=market_cap_desc&per_page=250&page=" + page + "&sparkline=false";
+//  let output = { error: null, body: null, statusCode: null, symbols: []};
 
-  res.setHeader('Content-Type', 'application/json');
-  request(url, options, (_error, _res, _body) => {
-    res.statusCode = _res.statusCode;
-    if (_error) {
-      res.end(_error);
-    }
-    else {
-      //res.end(JSON.stringify(_body));
-      let outputArray = [];
-      let j = [];
-      for (var i = 0; i < _body.length; i++) {
-        j = _body[i];
-        outputArray.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated});
-      }
-      res.end(JSON.stringify(outputArray));
-    }
-  });
-});
+//  request(url, options, (_error, _res, _body) =>
+//  {
+//    output.body = _body;
+//    output.error = _error;
+//    output.statusCode = _res.statusCode
+//    if (!_error) {
+//      let j = [];
+//      for (var i = 0; i < _body.length; i++) {
+//        j = _body[i];
+//        output.symbols.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
+//      }
+//    }
+//  });
+//}
 
-app.get('/coingecko/markets/usd/1/', (req, res) => {
-  const request = require('request');
-  url = host_coingecko + "v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false";
+function getCoinGeckoMarkets(page, vs_currency) {
+  return new Promise((resolve, reject) => {
+    let url = host_coingecko + "v3/coins/markets?vs_currency=" + vs_currency + "&order=market_cap_desc&per_page=250&page=" + page + "&sparkline=false";
+    output = {url: url, error: null, body: null, statusCode: null, symbols: [] };
 
-  res.setHeader('Content-Type', 'application/json');
-  request(url, options, (_error, _res, _body) => {
-    res.statusCode = _res.statusCode;
-    if (_error) {
-      res.end(_error);
-    }
-    else {
-      //res.end(JSON.stringify(_body));
-      let outputArray = [];
-      let j = [];
-      for (var i = 0; i < _body.length; i++) {
-        j = _body[i];
-        outputArray.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated});
-      }
-      res.end(JSON.stringify(outputArray));
-    }
-  });
-});
+    https.get(url, options, (response) => {
+      let chunks_of_data = [];
 
-app.get('/coingecko/markets/usd/', (req, res) =>
-{
-  const data = ['1']
+      response.on('data', (fragments) => {
+        chunks_of_data.push(fragments);
+      });
 
-  let requests = data.map(item => {
-    return fetch(host_coingecko + "v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${item}&sparkline=false", options);
-  });
+      response.on('end', () => {
+        let response_body = Buffer.concat(chunks_of_data);
+        output.body = response_body.toString();
+        output.statusCode = response.statusCode;
+        resolve(output);
+      });
 
-  Promise.all(requests).then((responses) =>
-  {
-
-  // Reason of using flat is, You will get array of responses like [[res1], [res2], [res3]]
-  // If you want to merge those together, You can use flat()
-    const data = responses[0].json();
-
-  // do whatever you want with results
-  let outputArray = [];
-  let j = [];
-    for (var i = 0; i < data.length; i++)
-    {
-  j = data[i];
-  outputArray.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
-  }
-
-  res.end(JSON.stringify(outputArray));
-  });
-});
-
-/*
-const request = require('request');
-
-function makeRequest(url) {
-    return new Promise((resolve, reject) => {
-        request(url, { json: true }, (err, res, body) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(body);
-            }
-        });
+      response.on('error', (error) => {
+        output.error = error;
+        reject(output);
+      });
     });
+  });
 }
 
-async function getCombinedData() {
-    try {
-        // Define your requests
-        let request1 = makeRequest('http://api.example.com/data1');
-        let request2 = makeRequest('http://api.example.com/data2');
-        let request3 = makeRequest('http://api.example.com/data3');
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-        // Use Promise.all to execute all requests in parallel
-        let responses = await Promise.all([request1, request2, request3]);
+// async function to make http request
+async function GetCoingeckoMarkets(vs_currency) {
+  try {
+    let output = { error: null, body: null, statusCode: null, symbols: [] };
+    let http_promise1 = getCoinGeckoMarkets(1, vs_currency);
+    let output1 = await http_promise1;
 
-        // Combine the data from the responses
-        let combinedData = {
-            data1: responses[0],
-            data2: responses[1],
-            data3: responses[2]
-        };
+    await sleep(1000);
 
-        console.log(combinedData);
-    } catch (error) {
-        console.error(error);
+    let http_promise2 = getCoinGeckoMarkets(2, vs_currency);
+    let output2 = await http_promise2;
+
+    await sleep(1000);
+
+    if (!output1.error) {
+      let j = [];
+      let json = JSON.parse(output1.body);
+      for (var i = 0; i < json.length; i++) {
+        j = json[i];
+        output.symbols.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
+      }
+      output.statusCode = output1.statusCode;
     }
+
+    if (!output2.error) {
+      let j = [];
+      let json = JSON.parse(output2.body);
+      for (var i = 0; i < json.length; i++) {
+        j = json[i];
+        output.symbols.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
+      }
+      output.statusCode = output2.statusCode;
+    }
+
+    return output;
+  }
+  catch (error) {
+    // Promise rejected
+    console.log(error);
+  }
 }
 
-getCombinedData();
+//app.get('/coingecko/markets/:vs_currency/:page/', (req, res) => {
+//  const request = require('request');
+//  const page = req.param('page');
+//  const vs_currency = req.param('vs_currency');
+//  let output = getCoinGeckoMarketsPage(page, vs_currency);
 
- */
-
-//app.get('/coingecko/markets/usd/', (req, res) => {
-//  //get TOP 1000 coins valued in USD
-//  //const request = require('request');
-//  //const runs = 4;
-//  //const sleep = async (milliseconds) =>
-//  //{
-//  //  await new Promise(resolve => setTimeout(resolve, milliseconds));
-//  //}
-//  const url1 = host_coingecko + "v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false";
-//  const url2 = host_coingecko + "v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=2&sparkline=false";
-//  const url3 = host_coingecko + "v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=3&sparkline=false";
-//  const url4 = host_coingecko + "v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=4&sparkline=false";
-
-//  const data1 = await got(url).json();
-//  //await sleep(2000);
-//  const data2 = await got(url).json();
-//  //await sleep(2000);
-//  //const data3 = await got(url).json();
-//  //await sleep(2000);
-//  //const data4 = await got(url).json();
-//  //await sleep(2000);
-//  //let data = data1.concat(data2).concat(data3).concat(data4);
-//  let data = data1.concat(data2);
-
-//  let j = [];
-//  for (var i = 0; i < data.length; i++) {
-//    j = data[i];
-//    outputArray.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
+//  res.setHeader('Content-Type', 'application/json');
+//  res.statusCode = output.statusCode;
+//  if (output.error) {
+//    res.end(output.error);
 //  }
-
-//  res.end(JSON.stringify(outputArray));
-
-//  //let curr_url = "";
-
-//  //res.setHeader('Content-Type', 'application/json');
-
-//  //let outputArray = [];
-
-//  //for (var i = 1; i <= runs; i++) {
-//  //  curr_url = url.replace("page=1", "page=" + i.toString());
-//  //  console.log('curr_url=' + curr_url);
-
-//  //  request(url, options, (_error, _res, _body) => {
-//  //    res.statusCode = _res.statusCode;
-//  //    if (_error) {
-//  //      console.log(_error);
-//  //      res.end(_error);
-//  //    }
-//  //    else
-//  //    {
-//  //      let j = [];
-//  //      for (var i = 0; i < _body.length; i++) {
-//  //        j = _body[i];
-//  //        outputArray.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
-//  //      }
-//  //    }
-//  //  });
-//  //}
-
-//  //res.end(JSON.stringify(outputArray));
-
-//  // send request A
-//  //got({uri: url})
-//  //  .then((responseA) => {
-//  //    // handle request A response
-//  //    let j = [];
-//  //    for (var i = 0; i < responseA.body _body.length; i++) {
-//  //    j = _body[i];
-//  //    outputArray.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
-
-//  //    // send request B
-//  //    return got({
-//  //      uri: "https://httpbin.org/json"
-//  //    })
-//  //  })
-//  //  .then((responseB) => {
-//  //    // handle request B response
-//  //    console.info(`${new Date().toISOString()}, responseB statusCode:`, responseB.statusCode)
-//  //  })
-//  //  .catch((error) => {
-//  //    console.error('catch error:', error);
-//  //  });
-
-//  //got({ uri: url })
-//  //.then()
+//  else {
+//    res.end(JSON.stringify(output.symbols));
+//  }
 //});
+
+app.get('/coingecko/markets/:vs_currency/', (req, res) => {
+  const request = require('request');
+  const vs_currency = req.param('vs_currency');
+
+  // anonymous async function to execute some code synchronously after http request
+  (async function () {
+    // wait to http request to finish
+    let output = await GetCoingeckoMarkets(vs_currency);
+
+    // below code will be executed after http request is finished
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = output.statusCode;
+    if (output.error) {
+      res.end(output.error);
+    }
+    else {
+      res.end(JSON.stringify(output.symbols));
+    }
+  })();
+});
+
+app.get('/coingecko/markets/:vs_currency/:page/', (req, res) => {
+  const request = require('request');
+  const page = req.param('page');
+  const vs_currency = req.param('vs_currency');
+  url = host_coingecko + "v3/coins/markets?vs_currency=" + vs_currency + "&order=market_cap_desc&per_page=250&page=" + page + "&sparkline=false";
+
+  res.setHeader('Content-Type', 'application/json');
+  request(url, options, (_error, _res, _body) => {
+    res.statusCode = _res.statusCode;
+    if (_error) {
+      res.end(_error);
+    }
+    else {
+      //res.end(JSON.stringify(_body));
+      let outputArray = [];
+      let j = [];
+      for (var i = 0; i < _body.length; i++) {
+        j = _body[i];
+        outputArray.push({ id: j.id, symbol: j.symbol, name: j.name, current_price: j.current_price, ath: j.ath, ath_date: j.ath_date, market_cap_rank: j.market_cap_rank, last_updated: j.last_updated });
+      }
+      res.end(JSON.stringify(outputArray));
+    }
+  });
+});
 
 app.listen(3001)
